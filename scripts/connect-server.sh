3f3c6cb61641d4dc81082b3205d2a5c746229db3
Fábio Lima Nunes <fabio.lima@moneycloud.com.br>
2025-07-25 11:02:03 -0300

    update script

--- scripts/connect-server.sh
+++ scripts/connect-server.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# IP do servidor remoto
-SERVER_IP="192.168.10.1"
+SERVER_IP="192.168.10.148"
+REMOTE_REPO_PATH="/home/fabio/Documentos/certification-cka/cka-study/configs"
 
-# Caminho do repositório no servidor remoto
-REMOTE_REPO_PATH="~/Documentos/certification-cka/cka-study/configs"
-
-# Solicita a senha do usuário
 read -sp "Digite a senha para o servidor SSH: " SERVER_PASSWORD
 echo
 
-# Conectando ao servidor remoto via SSH
 echo "Conectando ao servidor remoto no IP $SERVER_IP..."
-sshpass -p "$SERVER_PASSWORD" ssh fabio@$SERVER_IP << EOF
-    echo "Navegando até o repositório $REMOTE_REPO_PATH..."
-    cd $REMOTE_REPO_PATH
-
-    echo "Executando o comando 'vagrant ssh master-ssh1'..."
-    vagrant ssh master-ssh1 -c "sudo su -c 'export KUBECONFIG=/etc/kubernetes/admin.conf && bash'"
-EOFs
+sshpass -p "$SERVER_PASSWORD" ssh -t fabio@$SERVER_IP "cd $REMOTE_REPO_PATH && vagrant ssh master-1"
