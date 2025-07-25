e5e8fd50c4b97ceb41b9ef093402b4e4d159a0da
fabiolimanunes <fabiolimanunes@hotmail.com>
2025-07-25 10:19:57 -0300

    add script

--- configs/join-command.sh
+++ configs/join-command.sh
@@ -1,1 +1,1 @@
-kubeadm join 172.89.0.11:6443 --token qonu3k.h5tw903pgisnj8ts --discovery-token-ca-cert-hash sha256:dd13d6faa9512441465db50df300eb4cfb060b35df73870a4734b6f615ba097a 
+kubeadm join 172.89.0.11:6443 --token tibip8.dgma19k0vuxfh085 --discovery-token-ca-cert-hash sha256:a729c436c808f01f9f27a035ded6b5f5a8098d99318a15d157e9bf0d7489d428 
